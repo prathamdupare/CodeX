21d54fabe827b26c30a246758ebebde6eea74436
prathamdupare <prathamdupare@proton.me>
2024-04-21 17:38:11 +0530

    added authentication with clerk

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Button>Click me</Button>
+      <div className="flex flex-col bg-background h-screen w-full items-center justify-center px-2">
+        <Button className="">Click me</Button>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "./mode-toggle";
 
 const Navbar = async () => {
   return (
-    <div className="w-full flex items-center justify-between px-3 shadow mb-2 py-2 ">
+    <div className="w-full bg-background flex items-center justify-between px-3 shadow mb-2 py-2 ">
       <div>AcademiaX</div>
 
       <div className="flex flex-row items-center gap-4">
