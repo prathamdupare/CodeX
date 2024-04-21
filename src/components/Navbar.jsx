fc01837d18158ab156ba78047f800c36d2a5984b
prathamdupare <prathamdupare@proton.me>
2024-04-21 16:59:05 +0530

    base structure and blog

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "./mode-toggle";
+
+const Navbar = async () => {
+  return (
+    <div className="w-full flex items-center justify-between px-3 shadow mb-2 py-2 ">
+      <div>AcademiaX</div>
+
+      <div className="flex flex-row items-center gap-4">
+        <ModeToggle />
+
+        <SignedIn>
+          {/* Mount the UserButton component */}
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          {/* Signed out users get sign in button */}
+          <SignInButton />
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ theme.config.js
@@ -0,0 +1,11 @@
+export default {
+  logo: <span>AcadimiaX Blog</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  github_link: {
+    title: "AcadimiaX",
+    href: "https://github.com/prathamdupare/AcademiaX",
+  },
+  // ... other theme options
+};
