1c57f2a2b2b28cf97e072e2bea4e529e8439a4e1
prathamdupare <prathamdupare@proton.me>
2024-04-22 17:50:42 +0530

    temporarily removed middleware

--- src/middleware.js
+++ src/middleware.js
@@ -6,6 +6,5 @@ export const config = {
   matcher: [
     "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
     "/", // Run middleware on index page
-    "/(api|trpc)(.*)",
   ], // Run middleware on API routes
 };
